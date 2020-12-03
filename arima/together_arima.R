44e4e53361716196d9e4de20d42ba4a7b5a3fa50
sohui96 <sohuig@sookmyung.ac.kr>
2020-12-03 22:27:51 +0900

    12/3 22:30 so2 - arima2

--- arima/together_arima.R
+++ arima/together_arima.R
@@ -3,8 +3,22 @@
 # 1 1 0 0 1 1 0 
 # arima(p,d,q)(P,D,Q)m
 
+
+
 arima.so2 <- list()
 for ( i in 1:34 ){
   load(paste0('./arima/arima_SO2_sgg', i, '.RData'))
   arima.so2[[i]] <- mod_lst[[1]]$arma
 }
+
+# 다시 저장용 arima2
+for ( i in 1:34 ){
+  load(paste0('./arima/arima_SO2_sgg', i, '.RData'))
+  saveRDS(mod_lst, file=paste0("./arima2/arima2_SO2_sgg",i,".rds"))
+  rm(list=ls())
+}
+
+# 불러오기
+mod_lst <- readRDS("./arima2/arima2_SO2_sgg5.rds")
+
+
